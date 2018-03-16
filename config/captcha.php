e903484dd65ffbc515d7f2864bfeeee2d0123845
a935732122 <32761231+a935732122@users.noreply.github.com>
2018-03-16 09:31:20 +0800

    Add files via upload

---
+++ config/captcha.php
@@ -0,0 +1,63 @@
+<?php
+/*
+"mews/captcha": "~2.0"  
+composer update --ignore-platform-reqs
+'providers' => [
+        // ...
+        Mews\Captcha\CaptchaServiceProvider::class,
+    ]
+     'aliases' => [
+        // ...
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
+    ]
+    php artisan vendor:publish
+
+生成验证码图片 url  captcha_src();   <img src="{{captcha_src()}}" alt="">
+生成img标签  captcha_img();
+//验证
+            $rules = ['captcha' => 'required|captcha'];
+            $validator = Validator::make(Input::all(), $rules);
+ */
+return [
+
+    'characters' => '2346789abcdefghjmnpqrtuxyzABCDEFGHJMNPQRTUXYZ',
+
+    'default'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+    ],
+
+    'flat'   => [
+        'length'    => 6,
+        'width'     => 160,
+        'height'    => 46,
+        'quality'   => 90,
+        'lines'     => 6,
+        'bgImage'   => false,
+        'bgColor'   => '#ecf2f4',
+        'fontColors'=> ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+        'contrast'  => -5,
+    ],
+
+    'mini'   => [
+        'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+    ],
+
+    'inverse'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+        'sensitive' => true,
+        'angle'     => 12,
+        'sharpen'   => 10,
+        'blur'      => 2,
+        'invert'    => true,
+        'contrast'  => -5,
+    ]
+
+];
